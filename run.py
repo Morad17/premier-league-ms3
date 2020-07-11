3a2632a82847edba44dd16f67827e03f0b606057
Morad17 <morad123@hotmail.co.uk>
2020-07-11 15:23:17 +0000

    connected premierLeague db from mongodb

--- run.py
+++ run.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'premierLeague'
+app.config["MONGO_URI"] = 'mongodb+srv://root:Password20@myfirstcluster-ay9fk.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
+@app.route('/get_tasks')
 def hello():
-    return 'Hello New Project'
+    return render_template("landing-page.html", tasks=mongo.db.tasks.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
